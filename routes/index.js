48e24210404dec27780164294684dfd35870f7d4
Егор Зеленский <i@khaos.su>
2016-04-23 18:19:51 +0300

    Initial commit of the first version of the project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+var git = require('nodegit-kit');
+var promise = require('promise');
+
+var reposConfig = require('../repositories.json');
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+	var repositories = [];
+    var promises = [];
+	reposConfig.forEach(function (item) {
+		promises.push(
+            git.open(item.path, { init: false })
+    			.then(function (repo) {
+    				// Get repository log.
+    				return git.log(repo, { sort: 'time' })
+    					.then(function (log) {
+    						repositories.push({ 
+    							name: item.name, 
+                                path: item.path, 
+                                description: item.description,
+    							log: log,
+    							isBroken: false 
+    						});
+    					});
+    			})
+    			.catch(function () {
+    				// No repository found.
+    				repositories.push({ 
+    						name: item.name, 
+                            path: item.path,
+                            description: item.description,
+    						log: [],
+    						isBroken: true 
+    					});
+    			}));
+	});
+
+	promise.all(promises)
+        .then(function () {
+            res.render('index', { repositories: repositories });
+        });
 });
 
 module.exports = router;
